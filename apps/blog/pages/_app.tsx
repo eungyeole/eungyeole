6d7fd0759c797b23f1ed714acd170ffc45ce5e87
안은결 <eungyeole@naver.com>
2023-01-20 03:20:49 +0900

    feat(blog): vercel/analytics 연결

--- apps/blog/pages/_app.tsx
+++ apps/blog/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { GlobalStyle, ThemeProvider } from "ui";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>eungyeole의 블로그</title>
       </Head>
 
+      <Analytics />
       <ThemeProvider>
         <GlobalStyle />
         <Component {...pageProps} />
