b6fa42dfc197a4258ebdf88a446f26b1185e8ba9
안은결 <eungyeole@naver.com>
2023-02-12 03:29:28 +0900

    fix(cdn): vercel build 오류 수정 3트

--- apps/cdn/src/routes/root.ts
+++ apps/cdn/src/routes/root.ts
@@ -1,5 +1,9 @@
 import { FastifyPluginAsync } from "fastify";
 
-const rootRouter: FastifyPluginAsync = async (fastify) => {};
+const rootRouter: FastifyPluginAsync = async (fastify) => {
+  fastify.get("/ping", async (_request, reply) => {
+    return reply.send("pong");
+  });
+};
 
 export default rootRouter;
