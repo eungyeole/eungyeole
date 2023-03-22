846f1a50668bd44cddce194db03058603228b574
안은결 <eungyeole@naver.com>
2023-03-23 01:01:19 +0900

    feat(blog-editor): categories 탭 추가

--- apps/blog-editor/src/components/worksapce/Header/index.tsx
+++ apps/blog-editor/src/components/worksapce/Header/index.tsx
@@ -21,6 +21,11 @@ const tabs = [
     path: "members",
   },
   {
+    type: "dropdown",
+    label: "Categories",
+    path: "categories",
+  },
+  {
     label: "Settings",
     path: "settings",
   },
@@ -42,79 +47,89 @@ const Header: FC = () => {
   );
 
   return (
-    <Container>
-      <Flex align="center" fullHeight>
-        <SelectWorkspacePopup
-          selectedWorkspace={data}
-          trigger={
-            <WorkspaceContainer gap={8} flex="1" align="center">
-              <Avatar src={data?.profileImage} name={data?.name} size="small" />
-              <Text color="gray800" size="large" weight="bold">
-                {data?.name}
-              </Text>
-            </WorkspaceContainer>
-          }
-        />
-        <Divider
-          direction="vertical"
-          style={{
-            boxSizing: "border-box",
-            height: "32px",
-            margin: "0 12px 0 0",
-          }}
-        />
+    <>
+      <Container>
+        <Flex align="center" fullHeight>
+          <SelectWorkspacePopup
+            selectedWorkspace={data}
+            trigger={
+              <WorkspaceContainer gap={8} flex="1" align="center">
+                <Avatar
+                  src={data?.profileImage}
+                  name={data?.name}
+                  size="small"
+                />
+                <Text color="gray800" size="large" weight="bold">
+                  {data?.name}
+                </Text>
+              </WorkspaceContainer>
+            }
+          />
+          <Divider
+            direction="vertical"
+            style={{
+              boxSizing: "border-box",
+              height: "32px",
+              margin: "0 12px 0 0",
+            }}
+          />
+          <Flex gap={8}>
+            {tabs.map((tab) => (
+              <Link
+                key={tab.label}
+                href={`/workspaces/${workspaceId}/${tab.path}`}
+              >
+                <CustomButton
+                  variant="quiet"
+                  size="small"
+                  selected={
+                    !!asPath.startsWith(
+                      `/workspaces/${workspaceId}/${tab.path}`
+                    )
+                  }
+                >
+                  {tab.label}
+                </CustomButton>
+              </Link>
+            ))}
+          </Flex>
+        </Flex>
+
         <Flex gap={8}>
-          {tabs.map((tab) => (
-            <Link
-              key={tab.label}
-              href={`/workspaces/${workspaceId}/${tab.path}`}
-            >
-              <CustomButton
-                variant="quiet"
+          <Dropdown
+            trigger={
+              <Button
                 size="small"
-                selected={
-                  !!asPath.startsWith(`/workspaces/${workspaceId}/${tab.path}`)
+                variant="quiet"
+                tailingIcon={
+                  <Icon color="gray600">
+                    <IoMdArrowDropdown />
+                  </Icon>
                 }
               >
-                {tab.label}
-              </CustomButton>
-            </Link>
-          ))}
+                <Avatar variant="circle" name="eungyeole" size="xsmall" />
+              </Button>
+            }
+            render={({ hide }) => (
+              <Dropdown.Menu position="right">
+                <Dropdown.Item danger>Sign out</Dropdown.Item>
+                <Dropdown.Item onClick={hide} danger>
+                  Sign out
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            )}
+          />
         </Flex>
-      </Flex>
-
-      <Flex gap={8}>
-        <Dropdown
-          trigger={
-            <Button
-              size="small"
-              variant="quiet"
-              tailingIcon={
-                <Icon color="gray600">
-                  <IoMdArrowDropdown />
-                </Icon>
-              }
-            >
-              <Avatar variant="circle" name="eungyeole" size="xsmall" />
-            </Button>
-          }
-          render={({ hide }) => (
-            <Dropdown.Menu position="right">
-              <Dropdown.Item danger>Sign out</Dropdown.Item>
-              <Dropdown.Item onClick={hide} danger>
-                Sign out
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          )}
-        />
-      </Flex>
-    </Container>
+      </Container>
+      <Blank />
+    </>
   );
 };
 
 export default Header;
 
 const Container = styled.header`
+  position: fixed;
   width: 100%;
   height: 55px;
   display: flex;
@@ -124,7 +139,14 @@ const Container = styled.header`
 
   padding-right: 12px;
 
+  background-color: ${({ theme }) => theme.colors.white};
+
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray200};
+`;
+
+const Blank = styled.div`
+  height: 55px;
+  width: 100%;
 `;
 
 const WorkspaceContainer = styled(Flex)`
