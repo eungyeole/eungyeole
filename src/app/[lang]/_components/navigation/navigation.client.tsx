056e211ddfb3d2e5c543c7d567abcb9c2ee47a02
안은결 <eungyeole@naver.com>
2025-10-17 19:40:06 +0900

    fix: tabs to link

--- src/app/[lang]/_components/navigation/navigation.client.tsx
+++ src/app/[lang]/_components/navigation/navigation.client.tsx
@@ -1,49 +1,72 @@
 "use client";
-import { Tabs, TabsList, TabsTab } from "@/components/ui/tabs";
+import { cn } from "@/components/ui/cn";
 import { Cuboid, SwatchBook, UserIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { NAVIGATION_TABS } from "./constant";
+import { useEffect, useRef } from "react";
+import { useIsClient } from "@/hooks/use-is-client";
 
 interface NavigationClientProps {
   lang: string;
 }
 
-const TABS = [
-  { label: "about", value: "/", icon: UserIcon },
-  { label: "article", value: "/article", icon: SwatchBook },
-  { label: "sandbox", value: "/sandbox", icon: Cuboid },
-];
-
 export const NavigationClient = ({ lang }: NavigationClientProps) => {
-  const router = useRouter();
+  const isClient = useIsClient();
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const activeTabElementRef = useRef<HTMLSpanElement>(null);
+
   const pathname = usePathname();
 
   const cleaned = pathname.replace(`/${lang}`, "") || "/";
   const path = cleaned.split("/")[1];
   const value = path ? `/${path}` : "/";
 
-  const handleValueChange = (value: string) => {
-    router.push(value === "/" ? `/${lang}` : `/${lang}${value}`);
-  };
+  useEffect(() => {
+    const container = containerRef.current;
+    if (value && container) {
+      const activeTabElement = activeTabElementRef.current;
+      if (activeTabElement) {
+        const { offsetLeft, offsetWidth } = activeTabElement;
+        const containerWidth = container.offsetWidth;
+
+        if (containerWidth === 0) return;
+
+        const clipLeft = (offsetLeft / containerWidth) * 100;
+        const clipRight =
+          100 - ((offsetLeft + offsetWidth) / containerWidth) * 100;
+
+        container.style.clipPath = `inset(0 ${clipRight.toFixed(
+          2
+        )}% 0 ${clipLeft.toFixed(2)}% round 17px)`;
+      }
+    }
+  }, [value]);
 
   return (
-    <>
-      {TABS.map((tab) => (
-        <Link key={tab.value} href={tab.value}>
-          <tab.icon />
-          {tab.label}
-        </Link>
-      ))}
-      <Tabs value={value} onValueChange={handleValueChange}>
-        <TabsList>
-          {TABS.map((tab) => (
-            <TabsTab key={tab.value} value={tab.value}>
-              <tab.icon />
-              {tab.label}
-            </TabsTab>
-          ))}
-        </TabsList>
-      </Tabs>
-    </>
+    <div
+      ref={containerRef}
+      aria-hidden
+      inert
+      className={cn(
+        "absolute top-0 left-0 w-full h-full",
+        "transition-all duration-300",
+        isClient ? "opacity-100" : "opacity-0"
+      )}
+    >
+      <div className="w-full flex bg-emerald-950 rounded-[17px] text-white">
+        {NAVIGATION_TABS.map((tab) => (
+          <span
+            key={tab.value}
+            className="cursor-pointer px-2 pr-2.5 py-1 font-medium text-sm flex items-center gap-1 [&_svg]:w-3 [&_svg]:h-3"
+            ref={tab.value === value ? activeTabElementRef : null}
+          >
+            <tab.icon />
+            {tab.label}
+          </span>
+        ))}
+      </div>
+    </div>
   );
 };
--- src/app/[lang]/_components/navigation/navigation.tsx
+++ src/app/[lang]/_components/navigation/navigation.tsx
@@ -1,11 +1,24 @@
 import { lang } from "next/root-params";
 import { NavigationClient } from "./navigation.client";
-import { Suspense } from "react";
+import { NAVIGATION_TABS } from "./constant";
+import Link from "next/link";
 
 export const Navigation = async () => {
   return (
-    <Suspense fallback={null}>
+    <div className="relative w-fit">
+      <div className="w-full flex">
+        {NAVIGATION_TABS.map((tab) => (
+          <Link
+            key={tab.value}
+            href={tab.value}
+            className="cursor-pointer px-2 pr-2.5 py-1 font-medium text-sm flex items-center gap-1 [&_svg]:w-3 [&_svg]:h-3"
+          >
+            <tab.icon />
+            {tab.label}
+          </Link>
+        ))}
+      </div>
       <NavigationClient lang={await lang()} />
-    </Suspense>
+    </div>
   );
 };
