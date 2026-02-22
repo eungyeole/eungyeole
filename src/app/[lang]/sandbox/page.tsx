5a54b35aff13d14fff29a40463f05d6209c89b08
안은결 <eungyeole@naver.com>
2026-02-22 17:02:37 +0900

    fix: build error

--- src/app/[lang]/sandbox/page.tsx
+++ src/app/[lang]/sandbox/page.tsx
@@ -1,17 +1,12 @@
-import Link from "next/link";
 import { Card } from "./_components/card/card";
 
 export default function Sandbox() {
   return <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
     {Array.from({ length: 10 }).map((_, index) => (
-      <Link
-        href={`/sandbox/${index}`}
-        key={index}
-      >
-        <Card>
-          adad
-        </Card>
-      </Link>
+      // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+      <Card key={index}>
+        adad
+      </Card>
     ))}
   </div>
 }
